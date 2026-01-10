721a2929c64412d751b2e06cb33c533982e10f7f
Stefan Rieken <stefanrieken@gmail.com>
2026-01-10 20:09:20 +0100

    fix multi line parsing; format code

--- src/parse.asm
+++ src/parse.asm
@@ -49,44 +49,44 @@ repl:
 ;
 
 parse:
-  tsx      ; Save stack bottom to know if we still have (sub)expression data going
+  tsx                   ; Save stack bottom to know if we still have (sub)expression data going
   stx stackbottom
 
-  lda #$FF ; Count args per subexpr on stack
-  pha      ; Start with -1 so we can start with an increment
+  lda #$FF              ; Count args per subexpr on stack
+  pha                   ; Start with -1 so we can start with an increment
 
 _next_line:
   jsr read_new_line
 
 _next_char:
   #next_char
-_have_char:  ; jump here if your last char may be the first of another arg
-  tax
+_have_char:             ; jump here if your last char may be the first of another arg
+  tax                   ; char to x
 ; increment num args
-  pla        ; increment num args
+  pla
   clc
   adc #1
   pha
 ; test for end of line
   tya
   cmp strlen
-  txa
-  bcc _no_eol
+  txa                   ; restore char to a
+  bcc _no_eol           ; if not end of line, continue to parsing
 
-_emit_toplevel_eval:
-  pla       ; pull n args
+; Check if open brackets left
+; If not, emit toplevel eval
+  pla                   ; pull n args
   sta arg1
-
-  tsx
+  tsx                   ; check stack size to detect open brackets
   txa
   cmp stackbottom
   beq _do_emit_eval
 _have_brackets_open:
-  lda #$42
-  jsr WriteCharacter
-  lda arg1        ; restore last arg count on stack
+  lda arg1              ; restore last arg count on stack
+  sec
+  sbc #1                ; we incremented it without parsing an argument; so retract that
   pha
-  jmp _next_line  ; and read another line (assuming we're here because end of line!)
+  jmp _next_line        ; and read another line (assuming we're here because end of line!)
 _do_emit_eval:
   ldx #PRIM_EVAL
   jsr emit_byte_cmd
@@ -125,9 +125,9 @@ _try_close_sub:
   pla       ; pull n args
   sta arg1
   pla
-  cmp #'('  ; matching bracket?
+  cmp #'('              ; matching bracket?
   beq _emit_eval
-  jsr syntax_error ; TODO retract emitted values in this line (save prgtop just like stackbottom)
+  jsr syntax_error      ; TODO retract emitted values in this line (save prgtop just like stackbottom)
   ldx stackbottom
   txs
   jmp parse
@@ -140,29 +140,29 @@ _emit_eval:
 _try_open_blk:
   cmp #'{'
   bne _try_close_blk
-  pha                ; for bracket matching
-  lda prgtop+1       ; push insertion point (minus one!) on stack
+  pha                   ; for bracket matching
+  lda prgtop+1          ; push insertion point (minus one!) on stack
   pha
   lda prgtop
   pha
-  lda #$FF            ; start new arg count
+  lda #$FF              ; start new arg count
   pha
-  ldx #PRIM_SKIPW     ; emit 'skip' instruction
-  jsr emit_word_cmd   ; value is now garbage; must be fixed in close
+  ldx #PRIM_SKIPW       ; emit 'skip' instruction
+  jsr emit_word_cmd     ; value is now garbage; must be fixed in close
   jmp _next_char
 _try_close_blk:
   cmp #'}'
   bne _try_number
   pla
   sta tmp
-  pla                 ; get insertion point from stack
+  pla                   ; get insertion point from stack
   sta arg2
   pla
   sta arg2+1
-  pla                 ; bracket match
+  pla                   ; bracket match
   cmp #'{'
   beq _insert_target
-  jsr syntax_error ; TODO retract emitted values in this line (save prgtop just like stackbottom)
+  jsr syntax_error  ; TODO retract emitted values in this line (save prgtop just like stackbottom)
   ldx stackbottom
   txs
   jmp parse
@@ -175,7 +175,7 @@ _insert_target:
   jsr emit_byte
   tya
   pha
-  ldy #1           ; pointer is to start of instr, so +1 for word arg
+  ldy #1                ; pointer is to start of instr, so +1 for word arg
   lda prgtop
   sta (arg2),y
   iny
@@ -192,15 +192,15 @@ _try_number:
 ; yes!
   sec
   sbc #'0'
-  sta multiplicand   ; use multiplicand for 2-byte number result
-  ldx #0             ; switch to x to preserve digit a bit longer
+  sta multiplicand      ; use multiplicand for 2-byte number result
+  ldx #0                ; switch to x to preserve digit a bit longer
   stx multiplicand+1
   ldx #10
-  stx tmp            ; default base 10
-  cmp #0             ; was given digit zero?
-  bne _more_digits   ; if not, don't expect '0x'
+  stx tmp               ; default base 10
+  cmp #0                ; was given digit zero?
+  bne _more_digits      ; if not, don't expect '0x'
   #next_char
-  cmp #'x' ; hex indicator; TODO only trigger on zero and also accept inputs a-f
+  cmp #'x'
   bne +
   lda #16
   sta tmp
@@ -212,66 +212,66 @@ _more_digits:
   cmp #'9'+1
   bcc _done_adjusting
   cmp #'a'
-  bcc _try_uppercase  ; if < 'a'
-  sbc #$20            ; adjust to uppercase
+  bcc _try_uppercase      ; if < 'a'
+  sbc #$20                ; adjust to uppercase
 _try_uppercase:
   cmp #'A'
   bcc _num_done
-  cmp #'Z'+1          ; should realistically be 'F', but hey, we may support other bases some day
-  bcs _num_done       ; not even in A-Z
+  cmp #'Z'+1              ; should realistically be 'F', but hey, we may support other bases some day
+  bcs _num_done           ; not even in A-Z
   sec
-  sbc #7              ; make 'A'..'Z' go next to '9'
+  sbc #7                  ; make 'A'..'Z' go next to '9'
 _done_adjusting:
   sec
-  sbc #'0'            ; now make it numeric
-  cmp tmp             ; digit < base?
-  bcs _num_done       ; if not, it's not a valid digit
-  pha                 ; push digit
-  tya                 ; save y reg
+  sbc #'0'                ; now make it numeric
+  cmp tmp                 ; digit < base?
+  bcs _num_done           ; if not, it's not a valid digit
+  pha                     ; push digit
+  tya                     ; save y reg
   pha
-  lda tmp             ; load base
-  ldy #5              ; max = base 16 = 5 bits
+  lda tmp                 ; load base
+  ldy #5                  ; max = base 16 = 5 bits
   jsr multiply_by_a_y_bits
-  pla                 ; restore y reg
+  pla                     ; restore y reg
   tay
-  pla                 ; and add new digit
+  pla                     ; and add new digit
   clc
   adc multiplicand
   sta multiplicand
   bcc _more_digits
   inc multiplicand
-  bcs _more_digits ; always taken
+  bcs _more_digits      ; always taken
 _num_done:
   ldx #PRIM_PUSHB
   jsr emit_optimized_cmd
-  dey              ; we kind of messed up the last non-digit char, but this is an easy fix
+  dey                   ; we kind of messed up the last non-digit char, but this is an easy fix
   jmp _next_char
 
 _nan:
 
 _parse_string:
-  cmp #$22         ; '"'
-  bne _parse_label ; if not a string, then a label
-  sta tmp          ; store quote to signal string
+  cmp #$22              ; '"'
+  bne _parse_label      ; if not a string, then a label
+  sta tmp               ; store quote to signal string
   #next_char
   jsr parse_string_or_label
   ldx #PRIM_STRB
   jsr emit_optimized_cmd
-  jmp _next_char   ; discard closing '"' that was already parsed
+  jmp _next_char        ; discard closing '"' that was already parsed
 
 _parse_label:
-  ldx #$20         ; space delimits label
+  ldx #$20              ; space delimits label
   stx tmp
   jsr parse_string_or_label
-  txa              ; x contains unique string index
+  txa                   ; x contains unique string index
   cmp #NUM_FIXED_STRINGS
   bcc _valid
-  jsr syntax_error ; TODO retract emitted values in this line
+  jsr syntax_error      ; TODO retract emitted values in this line
   ldx stackbottom
   txs
   jmp parse
 _valid:
-  adc #MAX_CORE+1 ; assuming valid prim, adjust to jump table offset
+  adc #MAX_CORE+1      ; assuming valid prim, adjust to jump table offset
 ;clc
 ;adc #$30
 ;jsr WriteCharacter
@@ -279,7 +279,7 @@ _valid:
 ;sbc #$30
   tax
   jsr emit_byte
-  jmp _next_char ; discard delimiting space
+  jmp _next_char        ; discard delimiting space
 
 ;
 ; Parse string or label
